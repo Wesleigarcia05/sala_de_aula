a6d70bd4dd9ffb5b4098d54a985624ad6865ddb9
WESLEI FERNANDES GARCIA <05673709099@senacrs.edu.br>
2026-09-16 21:46:43 -0300

    Exercicios basicos de condicionais

---
+++ exercicios/condicionais.py
@@ -0,0 +1,31 @@
+def fizz_buzz( numero:int):
+    if numero % 3 == 0: numero % 5 == 0
+
+
+    if numero % 3 == 0:
+        return "fizz"
+    
+    elif numero % 5 == 0:
+        return "buzz"
+    
+    
+    
+        return numero
+if __name__ == "__main__":
+    pass
+
+#EXERCICIO 1
+def verificar_maioridade(idade:int):
+    if idade >  18:
+        return "Maior de idade"
+    
+    if idade < 18:
+        return "Menor de idade"
+"Maior de idade" == 20
+"Menor de idade" == 15
+print(f" {20>18} maior de idade")
+print(f" {15<18} menor de idade")
+
+#EXERCICIO 2 
+
+     
