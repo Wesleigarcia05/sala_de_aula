f266f6969d4f5505fc18f16890c4298ba7e42005
WESLEI FERNANDES GARCIA <05673709099@senacrs.edu.br>
2026-09-17 21:52:10 -0300

    loop.py

---
+++ exercicios/Loop.py
@@ -0,0 +1,7 @@
+def dobrar(numeros:list):
+    for numero in numeros:
+        numero = numero * 2
+        print(numero)
+
+
+
--- exercicios/condicionais.py
+++ exercicios/condicionais.py
@@ -1,16 +1,13 @@
-def fizz_buzz( numero:int):
-    if numero % 3 == 0: numero % 5 == 0
-
-
-    if numero % 3 == 0:
-        return "fizz"
-    
+def fizz_buzz(numero:int):
+    if numero % 3 == 0 and numero % 5 == 0:
+        return "fizzbuzz"
     elif numero % 5 == 0:
         return "buzz"
-    
-    
-    
+    elif numero % 3 == 0:
+        return "fizz"
+    else:
         return numero
+    
 if __name__ == "__main__":
     pass
 
@@ -27,5 +24,14 @@ print(f" {20>18} maior de idade")
 print(f" {15<18} menor de idade")
 
 #EXERCICIO 2 
+def verificar_paridade(numero:int):
+    if numero % 2 == 0:
+        return "par"
+    else:
+    
+        return "impar"
+print(f"o numero é par")
+    
 
-     
+
+
